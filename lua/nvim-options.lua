5673b89f9e40b17ab87878e67c4b90aa88ca13ae
Florian Thamm <florian.thamm03+github@proton.me>
2026-04-17 22:45:39 +0200

    Change tab settings to use 2 spaces instead of 4

--- lua/nvim-options.lua
+++ lua/nvim-options.lua
@@ -10,8 +10,8 @@ vim.opt.cursorline = true
 vim.opt.scrolloff = 999
 
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
